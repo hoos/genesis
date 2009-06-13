e8fc38dcccb4b238aad84d741ded818f24ab0ec4
husseinb <husseinb@neogen-tech.com>
2009-06-13 02:12:53 +0100

    Added comments to scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# build.sh     A build script that wraps Ant.
+#
+# Author:      Hussein Badakhchani
+#
+# Description: This script is used to compile and create distributions of
+#              Genesis.  All the arguments that are supplied to this script
+#              are passed on to Ant.
 
 GENESIS_HOME=`/bin/env dirname $0`
 ANT_HOME=$GENESIS_HOME/thirdparty/apache-ant-1.7.1
