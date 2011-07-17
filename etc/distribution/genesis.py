94dd1e990deb16f6e23916aaacd5e769df6ac8e1
Hussein Badakhchani <husseinb@localhost.localdomain>
2011-07-17 19:22:43 +0100

    Fixed grinder analyse task

--- etc/distribution/genesis.py
+++ etc/distribution/genesis.py
@@ -11,12 +11,48 @@ from com.uk.genesis.action import *
 from com.uk.genesis.ant import *
 from com.uk.genesis.model import *
 
-test0 = Test(0, "Test Load Handles Valid File With Empty Model")
+# GenesisLoaderTest tests
+test1 = Test(1, "Test Load Handles Valid File With Empty Model")
+test2 = Test(2, "Test Load Throws File Not Found Exception For Empty XML")
+test3 = Test(3, "Test Load Rejects Invalid Model Reader")
+test4 = Test(4, "Test Load Creates Valid Model Reader Instance")
+test5 = Test(5, "Test Load Configures Configurable Model Reader Instance")
+test6 = Test(6, "Test Provides Action Factory For No Actions")
+test7 = Test(7, "Test Provides Action Factory For Actions")
+
+# ArchetypeFactoryImplTest tests
+test8 = Test(8, "Test Factory Returns Correct Item Count")
 
 class TestRunner:
     def __call__(self):
+       # GenesisLoaderTest
        genesisLoaderTest = GenesisLoaderTest()
        genesisLoaderTest.setUp()
-       request = test0.wrap(genesisLoaderTest)
-       result = request.testLoadHandlesValidFileWithEmptyModel();
+       # Test 1
+       testWrapper = test1.wrap(genesisLoaderTest)
+       testWrapper.testLoadHandlesValidFileWithEmptyModel();
+       # Test 2
+       testWrapper = test2.wrap(genesisLoaderTest)
+       testWrapper.testLoadThrowsFileNotFoundExceptionForEmptyXml();
+       # Test 3
+       testWrapper = test3.wrap(genesisLoaderTest)
+       testWrapper.testLoadRejectsInvalidModelReader();
+       # Test 4
+       testWrapper = test4.wrap(genesisLoaderTest)
+       testWrapper.testLoadCreatesValidModelReaderInstance();
+       # Test 5
+       testWrapper = test5.wrap(genesisLoaderTest)
+       testWrapper.testLoadConfiguresConfigurableModelReaderInstance();
+       # Test 6
+       testWrapper = test6.wrap(genesisLoaderTest)
+       testWrapper.testProvidesActionFactoryForNoActions();
+       # Test 7
+       testWrapper = test7.wrap(genesisLoaderTest)
+       testWrapper.testProvidesActionFactoryForActions();
 
+       # ArchetypeFactoryImplTest
+       archetypeFactoryImplTest = ArchetypeFactoryImplTest()
+       #archetypeFactoryImplTest.setUp()
+       # Test 8
+       testWrapper = test8.wrap(archetypeFactoryImplTest)
+       testWrapper.testFactoryReturnsCorrectItemCount();
