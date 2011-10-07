8d247c09823c7e967946abc9dac4398366a5782e
Robin Datta <robindatta@gmail.com>
2011-10-07 13:17:05 +0100

    GenesisNamespaceContext.java: fixed Checkstyle checks, regexp and whitespace warnings

--- src/com/uk/genesis/GenesisNamespaceContext.java
+++ src/com/uk/genesis/GenesisNamespaceContext.java
@@ -8,29 +8,29 @@ import javax.xml.XMLConstants;
 import javax.xml.namespace.NamespaceContext;
 
 public class GenesisNamespaceContext implements NamespaceContext {
-	private Map<String, String> mappedNamespaces = new HashMap<String, String>();
-	
-	public GenesisNamespaceContext() {
-		mappedNamespaces.put("genesis", "http://genesis.uk.com/schemas/1.0/genesis-root");
-	}
-	
-	public String getNamespaceURI(String prefix) {
-		if (mappedNamespaces.containsKey(prefix)) {
-			return mappedNamespaces.get(prefix);
-		}
-		
-		return XMLConstants.NULL_NS_URI;
-	}
+    private Map<String, String> mappedNamespaces = new HashMap<String, String>();
 
-	public String getPrefix(String s) {
-		throw new UnsupportedOperationException();
-	}
+    public GenesisNamespaceContext() {
+        mappedNamespaces.put("genesis", "http://genesis.uk.com/schemas/1.0/genesis-root");
+    }
 
-	public Iterator<String> getPrefixes(String s) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public void addNamespace(String prefix, String uri) {
-		mappedNamespaces.put(prefix, uri);
-	}
+    public String getNamespaceURI(final String prefix) {
+        if (mappedNamespaces.containsKey(prefix)) {
+            return mappedNamespaces.get(prefix);
+        }
+
+        return XMLConstants.NULL_NS_URI;
+    }
+
+    public String getPrefix(final String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterator<String> getPrefixes(final String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addNamespace(final String prefix, final String uri) {
+        mappedNamespaces.put(prefix, uri);
+    }
 }
