971ebcd7e2e1221a87b46bedbde404864fff5eee
Robin Datta <robindatta@gmail.com>
2011-10-07 14:10:01 +0100

    ConfigurableModelReader.java: fixed Checkstyle regexp and whitespace warnings

--- src/com/uk/genesis/model/ConfigurableModelReader.java
+++ src/com/uk/genesis/model/ConfigurableModelReader.java
@@ -5,14 +5,14 @@ package com.uk.genesis.model;
  * that allows the reader to be configured. Configuration information
  * includes details such as the XML element containing model data and
  * genesis directory root.
- * 
+ *
  * @author paul.jones
  */
 public interface ConfigurableModelReader extends ModelReader {
-	/**
-	 * Indicates that the model reader should configure itself using
-	 * data in the given environment.
-	 * @param env the environment
-	 */
-	void configure(GenesisEnvironment env);
+    /**
+     * Indicates that the model reader should configure itself using
+     * data in the given environment.
+     * @param env the environment
+     */
+    void configure(GenesisEnvironment env);
 }
