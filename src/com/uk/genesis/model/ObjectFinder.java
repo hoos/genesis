9f348bcfb9d2b000ef4bda906761c74c6436a576
Robin Datta <robindatta@gmail.com>
2011-10-07 17:09:07 +0100

    ObjectFinder.java: fixed Checkstyle checks, regexp, and whitespace warnings

--- src/com/uk/genesis/model/ObjectFinder.java
+++ src/com/uk/genesis/model/ObjectFinder.java
@@ -12,90 +12,90 @@ import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.resources.FileResource;
 
 public final class ObjectFinder {
-	public static String[] findObjectNames(File basePath, String path, String name) {
-		String normPath = normalisePath(path);
-		List<String> result = new ArrayList<String>();
-		
-		FileSet set = new FileSet();
-		
-		set.setProject(new Project());
-		set.setDir(basePath.getAbsoluteFile());
-		if (normPath != null && normPath.length() > 0) {
-			set.appendIncludes(new String[] { path.replaceAll("\\$\\{name\\}", "*") + "/" 
-					+ name.replaceAll("\\$\\{name\\}", "*") });
-		} else {
-			set.appendIncludes(new String[] { name.replaceAll("\\$\\{name\\}", "*") });
-		}
-		
-		Iterator<?> it = set.iterator();
-		while (it.hasNext()) {
-			FileResource res = (FileResource) it.next();
-			
-			// We have a candidate file. See if we can calculate the name for it
-			String foundName = res.getFile().getName();
-			String foundPath = res.getFile().getParentFile().getAbsolutePath();
-			
-			//String absPath = res.getFile().getAbsolutePath();
-			String absBase = basePath.getAbsolutePath();
-			
-			if (!foundPath.startsWith(absBase)) {
-				throw new RuntimeException("Discovered file not within base path");
-			}
-			
-			String relative = foundPath.substring(absBase.length());
-			String relativeNorm = normalisePath(relative);
-			
-			// We now have a found name and a found path
-			String nameFromPath = null;
-			String nameFromFile = null;
-			String guessedObjectName = null;
-			if (normPath != null && normPath.contains("${name}")) {
-				nameFromPath = findName(normPath, relativeNorm);
-				guessedObjectName = nameFromPath;
-			}
-			if (name.contains("${name}")) {
-				nameFromFile = findName(name, foundName);
-				guessedObjectName = nameFromFile;
-			}
-			
-			if (guessedObjectName == null) {
-				throw new IllegalArgumentException("Neither name nor path specify ${name} token, so name cannot be determined");
-			}
-			if (nameFromPath == null || nameFromFile == null || nameFromPath.equals(nameFromFile)) {
-				// We found a good candidate
-				result.add(guessedObjectName);
-			}
-		}
-		
-		return result.toArray(new String[result.size()]);
-	}
-	
-	private static String normalisePath(String path) {
-		if (path == null) {
-			return null;
-		}
-		
-		if (path.startsWith("/") || path.startsWith("\\")) {
-			path = path.substring(1);
-		}
-		if (path.endsWith("/") || path.endsWith("\\")) {
-			path = path.substring(0, path.length() - 1);
-		}
-		
-		// Normalise the separators on windows
-		path = path.replaceAll("\\\\", "/");
-		
-		return path;
-	}
-	
-	private static String findName(String pattern, String found) {
-		Pattern regex = Pattern.compile(pattern.replaceAll("\\$\\{name\\}", "(.*)"));
-		Matcher matcher = regex.matcher(found);
-		
-		if (!matcher.matches()) {
-			throw new IllegalArgumentException("Cannot match found file against base pattern");
-		}
-		
-		return matcher.group(1);
-	}
+    public static String[] findObjectNames(final File basePath, final String path, final String name) {
+        String normPath = normalisePath(path);
+        List<String> result = new ArrayList<String>();
+
+        FileSet set = new FileSet();
+
+        set.setProject(new Project());
+        set.setDir(basePath.getAbsoluteFile());
+        if (normPath != null && normPath.length() > 0) {
+            set.appendIncludes(new String[] {path.replaceAll("\\$\\{name\\}", "*") + "/"
+                    + name.replaceAll("\\$\\{name\\}", "*") });
+        } else {
+            set.appendIncludes(new String[] {name.replaceAll("\\$\\{name\\}", "*") });
+        }
+
+        Iterator<?> it = set.iterator();
+        while (it.hasNext()) {
+            FileResource res = (FileResource) it.next();
+
+            // We have a candidate file. See if we can calculate the name for it
+            String foundName = res.getFile().getName();
+            String foundPath = res.getFile().getParentFile().getAbsolutePath();
+
+            //String absPath = res.getFile().getAbsolutePath();
+            String absBase = basePath.getAbsolutePath();
+
+            if (!foundPath.startsWith(absBase)) {
+                throw new RuntimeException("Discovered file not within base path");
+            }
+
+            String relative = foundPath.substring(absBase.length());
+            String relativeNorm = normalisePath(relative);
+
+            // We now have a found name and a found path
+            String nameFromPath = null;
+            String nameFromFile = null;
+            String guessedObjectName = null;
+            if (normPath != null && normPath.contains("${name}")) {
+                nameFromPath = findName(normPath, relativeNorm);
+                guessedObjectName = nameFromPath;
+            }
+            if (name.contains("${name}")) {
+                nameFromFile = findName(name, foundName);
+                guessedObjectName = nameFromFile;
+            }
+
+            if (guessedObjectName == null) {
+                throw new IllegalArgumentException("Neither name nor path specify ${name} token, so name cannot be determined");
+            }
+            if (nameFromPath == null || nameFromFile == null || nameFromPath.equals(nameFromFile)) {
+                // We found a good candidate
+                result.add(guessedObjectName);
+            }
+        }
+
+        return result.toArray(new String[result.size()]);
+    }
+
+    private static String normalisePath(final String path) {
+        if (path == null) {
+            return null;
+        }
+
+        if (path.startsWith("/") || path.startsWith("\\")) {
+            path = path.substring(1);
+        }
+        if (path.endsWith("/") || path.endsWith("\\")) {
+            path = path.substring(0, path.length() - 1);
+        }
+
+        // Normalise the separators on windows
+        path = path.replaceAll("\\\\", "/");
+
+        return path;
+    }
+
+    private static String findName(final String pattern, final String found) {
+        Pattern regex = Pattern.compile(pattern.replaceAll("\\$\\{name\\}", "(.*)"));
+        Matcher matcher = regex.matcher(found);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Cannot match found file against base pattern");
+        }
+
+        return matcher.group(1);
+    }
 }
