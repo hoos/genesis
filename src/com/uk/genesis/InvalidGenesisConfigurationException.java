7d31fefb4d079aa1c115c6f22c920de3bddb68c7
Robin Datta <robindatta@gmail.com>
2011-10-07 13:25:39 +0100

    InvalidGenesisConfigurationException.java: fixed Checkstyle checks, regexp and whitespace warnings

--- src/com/uk/genesis/InvalidGenesisConfigurationException.java
+++ src/com/uk/genesis/InvalidGenesisConfigurationException.java
@@ -6,16 +6,16 @@ package com.uk.genesis;
  *
  */
 public class InvalidGenesisConfigurationException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1574738876869395761L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1574738876869395761L;
 
-	public InvalidGenesisConfigurationException(String message) {
-		super(message);
-	}
+    public InvalidGenesisConfigurationException(final String message) {
+        super(message);
+    }
 
-	public InvalidGenesisConfigurationException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public InvalidGenesisConfigurationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
