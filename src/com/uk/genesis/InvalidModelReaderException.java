f5c7f493363604676dbe2d6bb693d379a6183981
Robin Datta <robindatta@gmail.com>
2011-10-07 13:35:16 +0100

    InvalidModelReaderException.java: fixed Checkstyle checks, regexp and whitespace warnings

--- src/com/uk/genesis/InvalidModelReaderException.java
+++ src/com/uk/genesis/InvalidModelReaderException.java
@@ -4,22 +4,22 @@ package com.uk.genesis;
  * Exception indicating that a configured ModelReader is invalid.
  * This may be because the class doesn't exist, or is not actually
  * a model reader.
- * 
+ *
  * @author paul.jones
  * @author hussein.badakhchani
  */
-public class InvalidModelReaderException 
-		extends InvalidGenesisConfigurationException {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5881566038270151677L;
+public class InvalidModelReaderException
+        extends InvalidGenesisConfigurationException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -5881566038270151677L;
 
-	public InvalidModelReaderException(String message) {
-		super (message);
-	}
-	
-	public InvalidModelReaderException(String message, Exception ex) {
-		super (message, ex);
-	}
+    public InvalidModelReaderException(final String message) {
+        super(message);
+    }
+
+    public InvalidModelReaderException(final String message, final Exception ex) {
+        super(message, ex);
+    }
 }
