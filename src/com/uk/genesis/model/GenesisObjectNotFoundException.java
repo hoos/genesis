43203f933144c0327b477bd94288c571fcb359ee
Robin Datta <robindatta@gmail.com>
2011-10-10 15:20:13 +0100

    GenesisObjectNotFoundException.java: fixed Checkstyle checks, regexp and whitespace warnings

--- src/com/uk/genesis/model/GenesisObjectNotFoundException.java
+++ src/com/uk/genesis/model/GenesisObjectNotFoundException.java
@@ -2,22 +2,23 @@ package com.uk.genesis.model;
 
 /**
  * Exception thrown when a GenesisObject that doesn't exist is requested.
- * 
+ *
  * @author paul.jones
  * @author hussein.badakhchani
  */
 public class GenesisObjectNotFoundException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6531356846975616579L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6531356846975616579L;
 
-	public GenesisObjectNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public GenesisObjectNotFoundException(final String message,
+                                            final Throwable cause) {
+        super(message, cause);
+    }
 
-	public GenesisObjectNotFoundException(String message) {
-		super(message);
-	}
+    public GenesisObjectNotFoundException(final String message) {
+        super(message);
+    }
 }
