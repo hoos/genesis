3a4ff069ac23ca87e71078cf3b3c911e228690f4
Robin Datta <robindatta@gmail.com>
2011-10-10 15:36:44 +0100

    GenesisObjectValidationException.java: fixed Checkstyle checks, regexp, sizes and whitespace warnings

--- src/com/uk/genesis/model/GenesisObjectValidationException.java
+++ src/com/uk/genesis/model/GenesisObjectValidationException.java
@@ -2,21 +2,22 @@ package com.uk.genesis.model;
 
 /**
  * Exception used to indicate that a GenesisObject has failed validation.
- * 
+ *
  * @author paul.jones
  * @author hussein.badakhchani
  */
 public class GenesisObjectValidationException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 683473989795806548L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 683473989795806548L;
 
-	public GenesisObjectValidationException(String message) {
-		super(message);
-	}
-	
-	public GenesisObjectValidationException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public GenesisObjectValidationException(final String message) {
+        super(message);
+    }
+
+    public GenesisObjectValidationException(final String message,
+                                            final Throwable cause) {
+        super(message, cause);
+    }
 }
